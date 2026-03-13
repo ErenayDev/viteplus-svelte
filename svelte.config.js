416cb6a5790d025f02deea948b914000add20e01
ErenayDev <erenaydev@proton.me>
2026-03-13 21:49:21 +0300

    fix: gh-pages, vp check error

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,23 @@
-import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-static"; // @sveltejs/adapter-auto
+import pkg from "./package.json" with { type: "json" };
 
+const dev = process.env.NODE_ENV === "development";
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
     // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
     // See https://svelte.dev/docs/kit/adapters for more information about adapters.
-    adapter: adapter(),
+    adapter: adapter({
+      pages: "build",
+      assets: "build",
+      fallback: null,
+      precompress: false,
+      strict: false,
+    }),
+    paths: {
+      base: dev ? "" : `/${pkg.name}`,
+    },
   },
   vitePlugin: {
     dynamicCompileOptions: ({ filename }) =>
