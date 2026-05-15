e97bb60ee9d6a4789385aafe3df2f7bf2a1d9d16
ErenayDev <erenaydev@proton.me>
2026-05-15 21:46:14 +0300

    switch dark mode, update styles, add my website link :(

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,46 +1,45 @@
 <script lang="ts" module>
-    export function sum(getCount: () => number, initial: number = 0) {
-        return {
-            get value() {
-                return getCount() + initial;
-            },
-        };
-    }
+  export function sum(getCount: () => number, initial: number = 0) {
+    return {
+      get value() {
+        return getCount() + initial;
+      },
+    };
+  }
 </script>
 
 <script lang="ts">
-    let count = $state(0);
-    import svelte from "$lib/assets/svelte.svg";
-    import viteplus from "$lib/assets/viteplus.svg";
+  let count = $state(0);
+  import svelte from "$lib/assets/svelte.svg";
+  import viteplus from "$lib/assets/viteplus.svg";
 </script>
 
 <div class="container">
-    <div class="logos">
-        <img src={viteplus} alt="VitePlus" class="logo" />
-        <span class="plus">+</span>
-        <img src={svelte} alt="Svelte" class="logo" />
-    </div>
-    <h1>VitePlus + SvelteKit</h1>
-    <p class="description">
-        Modern, fast and optimized development environment
-    </p>
-    <div class="links">
-        <a
-            href="https://viteplus.dev"
-            target="_blank"
-            rel="noopener noreferrer"
-        >
-            VitePlus Documentation
-        </a>
-        <a
-            href="https://svelte.dev/docs/kit"
-            target="_blank"
-            rel="noopener noreferrer"
-        >
-            SvelteKit Documentation
-        </a>
-    </div>
-    <button onclick={() => (count = sum(() => count, 1).value)} class="counter">
-        Clicks: {count}
-    </button>
+  <div class="logos">
+    <img src={viteplus} alt="VitePlus" class="logo" />
+    <span class="plus">+</span>
+    <img src={svelte} alt="Svelte" class="logo" />
+  </div>
+  <h1>VitePlus + SvelteKit</h1>
+  <p class="description">Modern, fast and optimized development environment</p>
+  <div class="links">
+    <a href="https://viteplus.dev" target="_blank" rel="noopener noreferrer">
+      VitePlus Documentation
+    </a>
+    <a
+      href="https://svelte.dev/docs/kit"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      SvelteKit Documentation
+    </a>
+  </div>
+  <button onclick={() => (count = sum(() => count, 1).value)} class="counter">
+    Clicks: {count}
+  </button>
+
+  <center
+    >Made by <a href="https://erenaydev.com.tr" target="_blank">Erenay</a
+    ></center
+  >
 </div>
